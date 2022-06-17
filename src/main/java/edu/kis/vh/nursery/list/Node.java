8cd546120f0faa721708c9506851e321c576a3e4
BartkiewiczM <228684@edu.p.lodz.pl>
2022-06-17 16:42:43 +0200

    1.5.3-6

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,35 +1,66 @@
 package edu.kis.vh.nursery.list;
 
+/**
+ * Node class
+ */
 public class Node {
 
 	private int value;
 	private Node prev;
 	private Node next;
 
+	/**
+	 * Constructor
+	 * @param i
+	 */
 	public Node(int i) {
 		setValue(i);
 	}
 
+	/**
+	 * value Getter
+	 * @return
+	 */
 	public int getValue() {
 		return value;
 	}
 
+	/**
+	 * value setter
+	 * @param value
+	 */
 	public void setValue(int value) {
 		this.value = value;
 	}
 
+	/**
+	 * node getter
+	 * @return
+	 */
 	public Node getPrev() {
 		return prev;
 	}
 
+	/**
+	 * node setter
+	 * @param prev
+	 */
 	public void setPrev(Node prev) {
 		this.prev = prev;
 	}
 
+	/**
+	 * next getter
+	 * @return
+	 */
 	public Node getNext() {
 		return next;
 	}
 
+	/**
+	 * next setter
+	 * @param next
+	 */
 	public void setNext(Node next) {
 		this.next = next;
 	}
